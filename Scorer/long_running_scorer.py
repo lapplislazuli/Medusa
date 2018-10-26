31cc02ab235d612016f6421f828e22f01e7705d4
floxdeveloper <fritz66@alpenjodel.de>
2018-10-26 10:38:44 +0200

    Included generator with rgb color proportion

--- Scorer/long_running_scorer.py
+++ Scorer/long_running_scorer.py
@@ -27,6 +27,35 @@ def create_bytearray(width=64,height=64):
     casted = imarray.astype('uint8')
     return casted
 
+def create_bytearray_with_color_prop(width = 64, height = 64, prop_red = 33, prop_green = 33, prop_blue = 33):
+    #Initalize array with zeros
+    imarray = numpy.random.rand(width * height, 3) * 0
+    red_pixel = prop_red/100*width*height
+    green_pixel = prop_green/100*width*height
+    blue_pixel = prop_blue/100*width*height
+    for x in range(height):
+        for y in range(width):
+            if(red_pixel > 0):
+                imarray[x*width+y] = [255,0,0]
+                red_pixel-= 1
+            elif(green_pixel > 0):
+                imarray[x*width+y] = [0,255,0]
+                green_pixel-= 1
+            elif(blue_pixel > 0):
+                imarray[x*width+y] = [0,0,255]
+                blue_pixel-= 1
+            else:
+                #Wrong proportion --> white pixel
+                imarray[x*width+y] = [255,255,255]
+    
+    numpy.random.shuffle(imarray)
+    immatrix = numpy.random.rand(height, width, 3) * 0
+    for x in range(height):
+        for y in range(width):
+            immatrix[x,y] = imarray[x*width+y]
+    casted = immatrix.astype('uint8')
+    return casted
+
 def create_img_from_bytearray(bytearr, colorscheme='RGBA'):
     im=Image.fromarray(bytearr).convert(colorscheme)
     return im
