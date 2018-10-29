d7289c9e5b184793c35a51e1bf485ec564ae8966
Twonki <l.gutschon@web.de>
2018-10-29 09:44:56 +0100

    Changed Structure, added commented-headers, added Test-function and pointed to correct collections

--- Medusa/Training.py
+++ Medusa/Training.py
@@ -14,63 +14,9 @@ import matplotlib.image as mpimg
 import io
 # Also Requires pyyaml and h5py installed with pip
 
-uri = "mongodb://MedusaUser:P3R5EU?@applis.me/Medusa?authSource=Medusa"
-label2num = {"weak":1,"medium":2,"strong":3} 
-num2label = {1:"weak", 2:"medium", 3:"strong"}
-
-def getMedusaImageCollection():
-    mongoClient=pymongo.MongoClient(uri)
-    medusaDB = mongoClient["Medusa"]
-    imageCollection = medusaDB["Images"]
-    return imageCollection
-
-def get_Image_cursor():
-    imageCollection = getMedusaImageCollection()
-    cursor = imageCollection.find()
-    return cursor
-
-def get_next_n_samples(cursor,n = 100):
-    samples = []
-    for _ in range(n):
-        entry = cursor.next()
-        samples.append((entry["success"],entry["image"]))
-    return samples
-
-def create_model():
-    model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(64,64,3)), # We got 64x64x3 Values per Image 
-        keras.layers.Dense(128, activation=tf.nn.relu),
-        keras.layers.Dense(256, activation=tf.nn.relu),
-        keras.layers.Dense(128, activation=tf.nn.sigmoid), 
-        keras.layers.Dense(4, activation=tf.nn.softmax) #3 Classes: Weak, Medium, Strong
-    ])
-    model.compile(optimizer=tf.keras.optimizers.Adam(), 
-                loss=tf.keras.losses.sparse_categorical_crossentropy,
-                metrics=['accuracy'])
-
-    return model
-
-
-def img_to_bytearray(Image):
-    imgByteArr = io.BytesIO()
-    Image.save(imgByteArr, format='PNG')
-    imgByteArr = imgByteArr.getvalue()
-    return imgByteArr
-
-def create_img_from_mongoDBBytes(mongobytes, colorscheme='RGBA'):
-    image = Image.open(io.BytesIO(mongobytes))
-    return image
-
-def label_to_int(label):
-    if(label2num[label]):
-        return label2num[label]
-    
-def int_to_label(number):
-    if(num2label[number]):
-        return num2label[number]
     
 def train_model(batchsize=100):
-    cursor = get_Image_cursor()
+    cursor = get_Image_cursor(getMedusaTrainingCollection())
     model=create_model()
     data = get_next_n_samples(cursor,batchsize)
     images = []
@@ -86,9 +32,87 @@ def train_model(batchsize=100):
     model.fit(images,labels,epochs=5)
     return model
 
+def test_model(model, testSize=4000):
+    cursor = get_Image_cursor(getMedusaTestCollection())
+    data = get_next_n_samples(cursor,testSize)
+    images = []
+    labels = []
+    for i in range(testSize):
+        images.append(mpimg.pil_to_array(create_img_from_mongoDBBytes(data[i][1])))
+        labels.append(label_to_int(data[i][0]))
+    #List 2 Array
+    images = np.asarray(images)
+    #Removing AlphaValues (They're always 255)
+    images = images[:,:,:,0:3]
+    labels = np.asarray(labels)
+    test_loss, test_acc = model.evaluate(images,labels)
+    print('Test accuracy:', test_acc)
+    
+################# SetUp ###############################
+uri = "mongodb://MedusaUser:P3R5EU?@applis.me/Medusa?authSource=Medusa"
+label2num = {"weak":1,"medium":2,"strong":3} 
+num2label = {1:"weak", 2:"medium", 3:"strong"}
+
+
+def create_model():
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(64,64,3)), # We got 64x64x3 Values per Image 
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(256, activation=tf.nn.relu),
+        keras.layers.Dense(4, activation=tf.nn.softmax) #3 Classes: Weak, Medium, Strong
+    ])
+    model.compile(optimizer=tf.keras.optimizers.Adam(), 
+                loss=tf.keras.losses.sparse_categorical_crossentropy,
+                metrics=['accuracy'])
+
+    return model
+
+def label_to_int(label):
+    if(label2num[label]):
+        return label2num[label]
+    
+def int_to_label(number):
+    if(num2label[number]):
+        return num2label[number]
+################# Mongo Helpers #######################
+def getMedusaTrainingCollection():
+    mongoClient=pymongo.MongoClient(uri)
+    medusaDB = mongoClient["Medusa"]
+    TrainingCollection = medusaDB["Training"]
+    return TrainingCollection
+
+def getMedusaTestCollection():
+    mongoClient=pymongo.MongoClient(uri)
+    medusaDB = mongoClient["Medusa"]
+    TestCollection = medusaDB["Test"]
+    return TestCollection
+
+def get_Image_cursor(collection):
+    cursor = collection.find()
+    return cursor
+
+def get_next_n_samples(cursor,n = 100):
+    samples = []
+    for _ in range(n):
+        entry = cursor.next()
+        samples.append((entry["success"],entry["image"]))
+    return samples
+################# Save and Load #######################
 def save_model(model, name):
         if(model and name):
                 model.save(name)
 
 def load_model(path):
         keras.models.load_model(path)
+
+################## Image Alternation ####################
+
+def img_to_bytearray(Image):
+    imgByteArr = io.BytesIO()
+    Image.save(imgByteArr, format='PNG')
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
+
+def create_img_from_mongoDBBytes(mongobytes, colorscheme='RGBA'):
+    image = Image.open(io.BytesIO(mongobytes))
+    return image
