7bafdf334086b5293bd73ee0c89ee18af4d9233f
Twonki <l.gutschon@web.de>
2018-11-12 09:28:21 +0100

    Refactored using List Comprehensions

--- Medusa/TestAphrodite.py
+++ Medusa/TestAphrodite.py
@@ -5,45 +5,26 @@ import FeedAphrodite as FA
 ###### Plots and Tests #####
 
 def plot_distribution(model,n=1000):
+        plot_distribution_with_threshhold(model,0,n)
+
+def plot_distribution_with_threshhold(model,threshhold,n=1000):
         scores, imgs = FA.create_and_rate_n_images(model,n)
-        topscorer = []
-        for s in scores:
-                c,a = FA.get_highest_score_and_class(s)
-                topscorer.append(c)
-        plt.hist(x=topscorer, bins=43, color='#0504aa',
-                            alpha=0.7, rwidth=0.85)
+        highscores = [FA.get_highest_score_and_class(s) for s in scores]
+        threshholded = [c for c,a in highscores if a>threshhold]
+        plt.hist(x=threshholded, bins=43)
         plt.grid(axis='y', alpha=0.75)
         plt.xlabel('Label')
         plt.ylabel('Frequency')
         plt.title('Label Distribution')
         plt.show()
 
-def plot_distribution_with_threshhold(model,threshhold,n=10000):
+def plot_distribution_with_compared_threshhold(model,threshhold,n=1000):
         scores, imgs = FA.create_and_rate_n_images(model,n)
-        topscorer = []
-        for s in scores:
-                c,a = FA.get_highest_score_and_class(s)
-                if(a>threshhold):
-                        topscorer.append(c)
-        plt.hist(x=topscorer, bins=43, color='#0504aa',
-                            alpha=0.7, rwidth=0.85)
-        plt.grid(axis='y', alpha=0.75)
-        plt.xlabel('Label')
-        plt.ylabel('Frequency')
-        plt.title('Label Distribution')
-        plt.show()
-
-def plot_distribution_with_threshhold_diff(model,threshhold,n=1000):
-        scores, imgs = FA.create_and_rate_n_images(model,n)
-        threshholded = []
-        unthreshholded = []
-        for s in scores:
-                c,a = FA.get_highest_score_and_class(s)
-                unthreshholded.append(c)
-                if(a>threshhold):
-                        threshholded.append(c)
+        topscorer = [FA.get_highest_score_and_class(s) for s in scores]
+        threshholded = [c for c,a in topscorer if a > threshhold]
+        unthreshholded = [c for c,a in topscorer]
         plt.figure()
-        plt.hist([unthreshholded,threshholded], 43, stacked=True, normed='density')
+        plt.hist([unthreshholded,threshholded], 43)
         plt.xlabel('Label')
         plt.ylabel('Frequency')
         plt.title('Label Distribution')
@@ -63,9 +44,7 @@ def plot_prop_dist(model,n=1000):
 
 def plot_prop_dist_of_label(model,label,n=1000):
         scores, imgs = FA.create_and_rate_n_images(model,n)
-        labelScores = []
-        for s in scores:
-                labelScores.append(s[label])
+        labelScores = [s[label] for s in scores]
         plt.hist(labelScores, bins=20, normed='density')
         plt.xlabel('Prop')
         plt.ylabel('Frequency')
