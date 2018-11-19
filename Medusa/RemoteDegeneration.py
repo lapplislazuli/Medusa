758349285d7bad8035859c8f8411c8bfd89bf803
Twonki <l.gutschon@web.de>
2018-11-19 12:46:11 +0100

    Remote Degeneration takes an alternationfunction as argument, default to noise. added smoothed alternationfn. Added a lot of comments

--- Medusa/RemoteDegeneration.py
+++ Medusa/RemoteDegeneration.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-import tensorflow as tf
-from tensorflow import keras
-
 import MedusaMongo as MMongo
 import ImageGenerator as MImg
 
@@ -12,42 +9,69 @@ import time
 import Scorer as Scorer
 import ImageHelper as ImgHelper
 
-url = 'https://phinau.de/trasi'
-access_key = 'ehiefoveingereim3ooD2vo8reeb9ooz'
-credentials = {'key': access_key}
-
-def remoteDegenerate(image, decay = 0.01, iterations = 10):
+# Method requires: 
+#   An image (as 64x64x3 Uint8 Array), 
+#   a function to alter the image,
+#   a threshhold how much the image can be worse by every step
+#   The # of Iterations i want to (successfully) alter my image
+#   The # of loops which i want to do max
+# Scores an image, alters it, and keeps the altered image if its not that worse.
+# Method Logic will be documented in detail throughout the function
+def remoteDegenerate(image, alternationfn = _alterImage, decay = 0.01, iterations = 10, maxloops=2000):
+    # First: Check if the Credentials are correct and the image is detected
     initialResp = Scorer.send_ppm_image(image)
     if(initialResp.status_code!=200):
         return
-    totalLoops = 0
+    # Initialise Start-Variables from our first score
+    totalLoops = 0 #Counts all loops
+    it = 0 #Counts successfull loops
     lastImg = image
     lastScore = Scorer.get_best_score(initialResp.text)
-    
+    # To check if we put garbage in
     print("StartConfidence:",lastScore)
-    it = 0 #Counts successfull loops!
-    while(it<iterations and totalLoops<2000):
+
+    #We stop if we either reach our depth, or we exceed the maxloops
+    while(it<iterations and totalLoops<maxloops):
         totalLoops+=1
-        degenerated = _alterImage(lastImg.copy(),7)
+        # Alter the last image and score it
+        degenerated = alternationfn(lastImg.copy())
         degeneratedResp = Scorer.send_ppm_image(degenerated)
         degeneratedScore= Scorer.get_best_score(degeneratedResp.text)
         print("Score:",degeneratedScore,"Depth:",it, "Loop:" , totalLoops)
+        # If our score is acceptable (better than the set decay) we keep the new image and score
         if(degeneratedScore> lastScore-decay):
             lastImg=degenerated
             lastScore=degeneratedScore
             it+=1
+        #We are working remote, we need to take a short break
         time.sleep(1.1)
+    #We return the lastImg, this can be something not that good if we just reach maxloops!
     return lastScore,lastImg
 
+# Generates an (64x64x3) Image with small values. It can be subtracted/added to a normal image to noise it
+# Could be moved to ImgHelper/Generator. Kept it here for a while so noone needs to search it. 
 def _generate_noise(density,strength=10,width=64,height=64):
     noise = np.random.rand(width,height,3)
     noise -=0.5 # to run from [-0.5,0.5]
-    noise*=strength
+    noise*=strength # To have Values bigger than 1, everything else would dissapear
     noise = np.asarray(noise,dtype="int")
     return noise
-    
+
+# Takes an image, and puts some noise on it. 
+# Return the image
 def _alterImage(image,strength=8):
     noise = _generate_noise(0.5,strength)
-    degenerated = image+noise
-    degenerated = np.asarray(degenerated,dtype="uint8")
-    return degenerated
+    altered = image+noise
+    # Image must be reparsed in the valid data-range [0,255]
+    # Values smaller than 0 will be mapped to high values (e.g. -2 => 253)
+    altered = np.asarray(altered,dtype="uint8")
+    return altered
+
+# Takes an image, puts noise on it, and smooths it with gaussian filter
+def _alterImageWithSmooth(image,strength=25):
+    noise = _generate_noise(0.5,strength)
+    altered = image+noise
+    # The Gaussian filter is quite strong, so i've taken only a little sigma
+    altered = ndimage.filters.gaussian_filter(altered,2)
+    altered = np.asarray(altered,dtype="uint8")
+    return altered
