ec947d357e8a22aff61611a1d70d9b4c93397ce6
Twonki <l.gutschon@web.de>
2018-11-22 08:59:17 +0100

    removed deprecated stratified Training, added some comments, refactored colorpropped Img

--- Medusa/ImageGenerator.py
+++ Medusa/ImageGenerator.py
@@ -1,48 +1,19 @@
-from numpy import random
 import numpy
+from numpy import random
 
-import io
-from PIL import Image, ImageEnhance
 import ImageHelper as ImgHelper
 
 def create_image(width = 64, height = 64):
-    imarray = random.rand(width,height,3)
-    return imarray
+    return random.rand(width,height,3).astype('uint8')
 
 def create_n_images(n):
-    batch = []
-    for i in range(n):
-        batch.append(create_image())
-    return numpy.asarray(batch)
-
-def __propFilter(value,prop):
-    return value if(numpy.random.rand()<prop) else 0
+    return numpy.asarray([create_image() for i in range(n)])
 
 def create_image_with_color_prop(width = 64, height = 64, prop_red = 33, prop_green = 34, prop_blue = 33):
   return ImgHelper.create_img_from_bytearray(create_bytearray_with_color_prop(width, height, prop_red, prop_green, prop_blue), 'RGBA')
 
-def _create_red_image_part(prop):
-    imarray = numpy.asarray([(__propFilter(redpart,prop),0,0) for redpart in numpy.random.rand(64*64)*255])
-    imarray.shape=(64,64,3)
-    return imarray.astype('uint8')
-
-def _create_green_image_part(prop):
-    imarray = numpy.asarray([(0,__propFilter(greenpart,prop),0) for greenpart in numpy.random.rand(64*64)*255])
-    imarray.shape=(64,64,3)
-    return imarray.astype('uint8')
-
-def _create_blue_image_part(prop):
-    imarray = numpy.asarray([(0,0,__propFilter(bluepart,prop)) for bluepart in numpy.random.rand(64*64)*255])
-    imarray.shape=(64,64,3)
-    return imarray.astype('uint8')
-
-def create_bytearray_with_color_prop(width = 64, height = 64, prop_red = 1, prop_green = 1, prop_blue = 1):
-    return _create_red_image_part(prop_red)+_create_blue_image_part(prop_blue)+_create_green_image_part(prop_green)
-
-#From Scorer
-def create_bytearray(width=64,height=64):
-    imarray = random.rand(width,height,3)*255
-    casted = imarray.astype('uint8')
-    return casted
-
-
+def create_bytearray_with_color_prop(redProp,blueProp,greenProp,width=64,heigth=64):
+    redPart   = numpy.asarray([v if redProp>numpy.random.rand() else 0 for v in numpy.random.rand(width*heigth)*255]).reshape((width,heigth))
+    bluePart  = numpy.asarray([v if blueProp>numpy.random.rand() else 0 for v in numpy.random.rand(width*heigth)*255]).reshape((width,heigth))
+    greenPart = numpy.asarray([v if greenProp>numpy.random.rand() else 0 for v in numpy.random.rand(width*heigth)*255]).reshape((width,heigth))
+    return numpy.asarray([redPart,bluePart,greenPart]).transpose(1,2,0).astype('uint8')
--- Medusa/ImageHelper.py
+++ Medusa/ImageHelper.py
@@ -4,7 +4,6 @@ import io
 
 import matplotlib.image as mpimg
 
-# IMG MANIPULATION
 from scipy import ndimage
 from scipy import misc as scipyMisc
 
@@ -15,10 +14,29 @@ def prepareTrafficSigns(images,labels):
     rimages = rimages/255 # Change to float
     return rimages,rlabels
 
-def load_ppm_image(path):
-    i = mpimg.imread(path)
-    a = scipyMisc.imresize(i, (64,64,3), interp='bicubic', mode=None)
-    return a
+# Loads an ppm image as bytearray from a path and resizes it to our required shape (64,64,3) 
+# The bicubic interpolation looks "best" imo, others are 'nearest' and 'bilinear'
+# Used for AphroditeTraining and in Degeneration 
+def load_ppm_image(path, interpolation = 'bicubic'):
+    raw = mpimg.imread(path)
+    return scipyMisc.imresize(raw, (64,64,3), interp=interpolation, mode=None)
+
+################## Image Alternation ####################
+# Creates the required bytearray for trasi webinterface
+# Sadly it doesnt take our nice numpy bytearrays, but requires a binary saved png
+# Therefore we store it to an in memory stream as byte, and read the in memory stream
+def img_to_bytearray(Image):
+    imgByteArr = io.BytesIO()
+    Image.save(imgByteArr, format='PNG')
+    return imgByteArr.getvalue()
+
+# Takes the image blob from MongoDB and parses it to a PIL Img
+def create_img_from_mongoDBBytes(mongobytes, colorscheme='RGBA'):
+    return Image.open(io.BytesIO(mongobytes))
+
+# Takes and uint Rgb bytearray and outputs PIL Image
+def create_img_from_bytearray(bytearr, colorscheme='RGBA'):
+    return Image.fromarray(bytearr).convert(colorscheme)
 
 def change_brigthness_of_img(image, brightnessFactor=1.0):
     changedImage = ImageEnhance.Brightness(image).enhance(brightnessFactor)
@@ -27,19 +45,3 @@ def change_brigthness_of_img(image, brightnessFactor=1.0):
 def change_contrast_of_img(image, contrastFactor=1.0):
     changedImage = ImageEnhance.Contrast(image).enhance(contrastFactor)
     return changedImage
-
-################## Image Alternation ####################
-
-def img_to_bytearray(Image):
-    imgByteArr = io.BytesIO()
-    Image.save(imgByteArr, format='PNG')
-    imgByteArr = imgByteArr.getvalue()
-    return imgByteArr
-
-def create_img_from_mongoDBBytes(mongobytes, colorscheme='RGBA'):
-    image = Image.open(io.BytesIO(mongobytes))
-    return image
-
-def create_img_from_bytearray(bytearr, colorscheme='RGBA'):
-    im=Image.fromarray(bytearr).convert(colorscheme)
-    return im
