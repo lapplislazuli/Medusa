c6ba8bba19490d5d2e2aaefdbef4043bddffa12d
floxdeveloper <fritz66@alpenjodel.de>
2018-12-07 08:42:59 +0100

    Changed definition order and added missing import

--- Medusa/Degeneration.py
+++ Medusa/Degeneration.py
@@ -7,67 +7,7 @@ import ImageHelper as ImgHelper
 import MedusaMongo as MMongo
 import ImageGenerator as MImg
 
-############################### Remote #######################################
-# This Methods runs Remote - the local Degeneration is Beneath
-
-# Method requires: 
-#   An image (as 64x64x3 Uint8 Array), 
-#   a function to alter the image,
-#   a threshhold how much the image can be worse by every step
-#   The # of Iterations i want to (successfully) alter my image
-#   The # of loops which i want to do max
-# Scores an image, alters it, and keeps the altered image if its not that worse.
-# Method Logic will be documented in detail throughout the function
-def remoteDegenerate(image, alternationfn = _noise, decay = 0.01, iterations = 10, maxloops=2000):
-    # First: Check if the Credentials are correct and the image is detected
-    initialResp = Scorer.send_ppm_image(image)
-    if(initialResp.status_code!=200):
-        return
-    # Initialise Start-Variables from our first score
-    totalLoops = 0 #Counts all loops
-    depth = 0 #Counts successfull loops
-    lastImg = image
-    lastScore = Scorer.get_best_score(initialResp.text)
-    # To check if we put garbage in
-    print("StartConfidence:",lastScore)
-
-    #We stop if we either reach our depth, or we exceed the maxloops
-    while(depth<iterations and totalLoops<maxloops):
-        totalLoops+=1
-        # Alter the last image and score it
-        degenerated = alternationfn(lastImg.copy())
-        degeneratedResp = Scorer.send_ppm_image(degenerated)
-        degeneratedScore= Scorer.get_best_score(degeneratedResp.text)
-        print("Score:",degeneratedScore,"Depth:",depth, "Loop:" , totalLoops)
-        # If our score is acceptable (better than the set decay) we keep the new image and score
-        if(degeneratedScore> lastScore-decay):
-            lastImg=degenerated
-            lastScore=degeneratedScore
-            depth+=1
-        #We are working remote, we need to take a short break
-        time.sleep(1.1)
-    #We return the lastImg, this can be something not that good if we just reach maxloops!
-    return lastScore,lastImg
-
-################### Local ######################
-# This Degeneration runs for local Models
-# Procedere is nearly the same as above
-def degenerate (model, image, label, alternationfn = _noise, iterations=10, decay = 0.01, maxloops=2000):
-    totalLoops = 0
-    depth = 0
-    lastScores,lastImage = predict_single_image(model,image)
-    lastLabelScore=lastScores[label]
-    
-    while(depth<iterations and totalLoops<maxloops):
-        totalLoops+=1
-        degenerated = alternationfn(lastImage.copy())
-        degScores,degImage = predict_single_image(model,degenerated)
-        degLabelScore=degScores[label]
-        if(degLabelScore> lastLabelScore-decay):
-            lastImage=degImage
-            lastLabelScore=degLabelScore
-            depth+=1
-    return lastLabelScore,lastImage
+from scipy import ndimage
 
 ################### Helpers #####################
 
@@ -132,3 +72,65 @@ def _generate_noise(density,strength=10,width=64,height=64):
     noise*=strength # To have Values bigger than 1, everything else would dissapear
     noise = np.asarray(noise,dtype="int")
     return noise
+
+############################### Remote #######################################
+# This Methods runs Remote - the local Degeneration is Beneath
+
+# Method requires: 
+#   An image (as 64x64x3 Uint8 Array), 
+#   a function to alter the image,
+#   a threshhold how much the image can be worse by every step
+#   The # of Iterations i want to (successfully) alter my image
+#   The # of loops which i want to do max
+# Scores an image, alters it, and keeps the altered image if its not that worse.
+# Method Logic will be documented in detail throughout the function
+def remoteDegenerate(image, alternationfn = _noise, decay = 0.01, iterations = 10, maxloops=2000):
+    # First: Check if the Credentials are correct and the image is detected
+    initialResp = Scorer.send_ppm_image(image)
+    if(initialResp.status_code!=200):
+        return
+    # Initialise Start-Variables from our first score
+    totalLoops = 0 #Counts all loops
+    depth = 0 #Counts successfull loops
+    lastImg = image
+    lastScore = Scorer.get_best_score(initialResp.text)
+    # To check if we put garbage in
+    print("StartConfidence:",lastScore)
+
+    #We stop if we either reach our depth, or we exceed the maxloops
+    while(depth<iterations and totalLoops<maxloops):
+        totalLoops+=1
+        # Alter the last image and score it
+        degenerated = alternationfn(lastImg.copy())
+        degeneratedResp = Scorer.send_ppm_image(degenerated)
+        degeneratedScore= Scorer.get_best_score(degeneratedResp.text)
+        print("Score:",degeneratedScore,"Depth:",depth, "Loop:" , totalLoops)
+        # If our score is acceptable (better than the set decay) we keep the new image and score
+        if(degeneratedScore> lastScore-decay):
+            lastImg=degenerated
+            lastScore=degeneratedScore
+            depth+=1
+        #We are working remote, we need to take a short break
+        time.sleep(1.1)
+    #We return the lastImg, this can be something not that good if we just reach maxloops!
+    return lastScore,lastImg
+
+################### Local ######################
+# This Degeneration runs for local Models
+# Procedere is nearly the same as above
+def degenerate (model, image, label, alternationfn = _noise, iterations=10, decay = 0.01, maxloops=2000):
+    totalLoops = 0
+    depth = 0
+    lastScores,lastImage = predict_single_image(model,image)
+    lastLabelScore=lastScores[label]
+    
+    while(depth<iterations and totalLoops<maxloops):
+        totalLoops+=1
+        degenerated = alternationfn(lastImage.copy())
+        degScores,degImage = predict_single_image(model,degenerated)
+        degLabelScore=degScores[label]
+        if(degLabelScore> lastLabelScore-decay):
+            lastImage=degImage
+            lastLabelScore=degLabelScore
+            depth+=1
+    return lastLabelScore,lastImage
