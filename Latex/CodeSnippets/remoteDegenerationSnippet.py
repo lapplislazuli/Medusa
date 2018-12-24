c329a33cbef0b0bf00522d1696e639226561904b
Twonki <l.gutschon@web.de>
2018-12-24 15:24:18 +0100

    Added RemoteDegCode and python highlighting

---
+++ Latex/CodeSnippets/remoteDegenerationSnippet.py
@@ -0,0 +1,50 @@
+# Parameters: 
+#   An image (as 64x64x3 Uint8 Array), 
+#   a function to alter the image,
+#   a threshold how much the image can be worse by every step
+#   The # of Iterations i want to (successfully) alter my image
+#   The # of loops which i want to do max
+def remoteDegenerate(image, alternationfn = _noise, decay = 0.01, iterations = 10, maxloops=2000, verbose=True, history=True):
+    # First: Check if the credentials are correct and the image is detected
+    initialResp = Scorer.send_ppm_image(image)
+    if(initialResp.status_code!=200):
+        return
+    totalLoops = 0 # Counts all loops
+    depth = 0 # Counts successfull loops
+    lastImage = image
+    lastScore = Scorer.get_best_score(initialResp.text)
+    # To check if we put garbage in
+    print("StartConfidence:",lastScore)
+    if history:
+        h = []    
+    #We stop if we either reach our depth, or we exceed the maxloops
+    while(depth<iterations and totalLoops<maxloops):
+        totalLoops+=1
+        # Alter the last image and score it
+        degenerated = alternationfn(lastImage.copy())
+        degeneratedResp = Scorer.send_ppm_image(degenerated)
+        if (degeneratedResp.status_code==200):
+            degeneratedScore= Scorer.get_best_score(degeneratedResp.text)
+        else:
+            print("Error, status code was: ", degeneratedResp.status_code)
+            #Attempts do not count
+            totalLoops-=1
+        # if we verbose, we want console output (then we see directly if anything is not working)
+        if verbose:
+            print("Score:",degeneratedScore,"Depth:",depth, "Loop:" , totalLoops)
+        # if we have history=True, we collect the same data as in verbose to plot something nice
+        if history:
+            h.append((degeneratedScore,depth,totalLoops))
+        # If our score is acceptable (better than the set decay) we keep the new image and score
+        if(degeneratedScore>=lastScore-decay):
+            lastImage=degenerated
+            lastScore=degeneratedScore
+            depth+=1
+        #We are working remote, we need to take a short break
+        time.sleep(1.1)
+    print("FinalConfidence:",lastScore)
+    if h!=[] :
+        plotHistory(h)
+        return lastScore,lastImage,h
+    else:
+        return lastScore,lastImage
